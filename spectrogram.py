95a81d381a6904263a91b3d176eab5436c77384a
stanislavlyalin <lyalinstas@gmail.com>
2018-12-20 00:13:11 +0300

    добавлена таблица нот по всем октавам

--- spectrogram.py
+++ spectrogram.py
@@ -4,7 +4,7 @@ from settings import FFT_SIZE
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPainter, QImage
 from PyQt5.QtWidgets import QWidget
-from utils import adobe_palette, freqToPx
+from utils import adobe_palette, freqToPx, notes, note_name
 
 
 class Spectrogram(QWidget):
@@ -12,7 +12,6 @@ class Spectrogram(QWidget):
         super().__init__()
         self.data = np.zeros((300, FFT_SIZE // 2))
         self.pal = adobe_palette()
-        self.notes = [['C', 261.63], ['D', 293.66], ['E', 329.63], ['F', 349.23], ['G', 392.00], ['A', 440.00], ['B', 493.88]]
     
     def plot(self, data):
         self.data = np.vstack((self.data[1:,:], data))
@@ -22,6 +21,7 @@ class Spectrogram(QWidget):
         palette_indexes = list(self.data.astype(np.uint8))
         image_data = self.pal[palette_indexes]
 
+        # отрисовка спектрограммы
         im = QImage(image_data, self.data.shape[1], self.data.shape[0], QImage.Format_RGB32)
         im = im.scaled(self.width(), self.height(), Qt.IgnoreAspectRatio, Qt.SmoothTransformation)
         p = QPainter(self)
@@ -29,7 +29,7 @@ class Spectrogram(QWidget):
 
         # отрисовка нот
         p.setPen(Qt.white)
-        for n in self.notes:
-            x = freqToPx(n[1], self.width())
+        for i, note in enumerate(notes(4)):
+            x = freqToPx(note, self.width())
             p.drawLine(x, 30, x, self.height())
-            p.drawText(x-10, 0, 20, 20, Qt.AlignHCenter, n[0])
+            p.drawText(x-10, 0, 20, 20, Qt.AlignHCenter, note_name(i))
